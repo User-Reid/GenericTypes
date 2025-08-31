63c0e3cbab5e07eb54ba6c66819d78a4ef485d71
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-31 13:33:07 -0500

    understanding how Lists work under the hood :)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿var numbers = new List<int> { 1, 3, 1, 2, 41 };
-var words = new List<string> { "aaa", "bbb" };
+var words = new List<string> { "aaa", "bbb", "ccc" };
 var dates = new List<DateTime> { new DateTime(2023, 1, 4) };
 
 Console.ReadKey();
