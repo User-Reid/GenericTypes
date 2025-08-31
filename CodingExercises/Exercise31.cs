cb73980924a5313b9591d22f8f968cdb7c4c3348
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-31 15:33:23 -0500

    completed coding assignment, passed everywhere except learned to use the private setter.

---
+++ CodingExercises/Exercise31.cs
@@ -0,0 +1,23 @@
+// var pairOfInts = new Pair<int>(1, 2);
+// System.Console.WriteLine($"First is {pairOfInts.First}");
+// System.Console.WriteLine($"Second is {pairOfInts.Second}");
+// pairOfInts.ResetFirst();
+
+// public class Pair<T>
+// {
+//   public T First { get; private set; }
+//   public T Second { get; private set; }
+//   public Pair(T first, T second) {
+//     First = first;
+//     Second = second;
+//   }
+
+//   public void ResetFirst() {
+//      First = default;
+//   }
+
+//   public void ResetSecond() {
+//     Second = default;
+//   }
+// };
+
