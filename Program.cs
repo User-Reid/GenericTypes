59856a929132eca2f05d7719e403541da0ae393c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-31 19:40:32 -0500

    understanding the need for Tuples :)

--- Program.cs
+++ Program.cs
@@ -1,68 +1,111 @@
-﻿var numbers = new SimpleList<int>();
-numbers.Add(10);
-numbers.Add(20);
-numbers.Add(30);
-numbers.Add(40);
-numbers.Add(50);
+﻿// var numbers = new SimpleList<int>();
+// numbers.Add(10);
+// numbers.Add(20);
+// numbers.Add(30);
+// numbers.Add(40);
+// numbers.Add(50);
 
-numbers.RemoveAt(2);
+// numbers.RemoveAt(2);
 
-var words = new SimpleList<string>();
-words.Add("aaa");
-words.Add("bbb");
-words.Add("ccc");
+// var words = new SimpleList<string>();
+// words.Add("aaa");
+// words.Add("bbb");
+// words.Add("ccc");
 
-var dates = new SimpleList<DateTime>();
-dates.Add(new DateTime(2025, 1, 6));
-dates.Add(new DateTime(2025, 1, 3));
+// var dates = new SimpleList<DateTime>();
+// dates.Add(new DateTime(2025, 1, 6));
+// dates.Add(new DateTime(2025, 1, 3));
+
+// Console.ReadKey();
+
+// public class SimpleList<T>
+// {
+//   private T[] _items = new T[4];
+//   private int _size = 0;
+
+//   public void Add(T item)
+//   {
+//     if (_size >= _items.Length)
+//     {
+//       var newItems = new T[_items.Length * 2];
+
+//       for (int i = 0; i < _items.Length; i++)
+//       {
+//         newItems[i] = _items[i];
+//       }
+//       _items = newItems;
+//     }
+//     _items[_size] = item;
+//     ++_size;
+//   }
+
+//   public void RemoveAt(int index)
+//   {
+//     if (index < 0 || index > _size)
+//     {
+//       throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+//     }
+
+//     --_size;
+
+//     for (int i = index; i < _size; ++i)
+//     {
+//       _items[i] = _items[i + 1];
+//     }
+
+//     _items[_size] = default;
+//   }
+
+//   public T GetAtIndex(int index)
+//   {
+//     if (index < 0 || index > _size)
+//     {
+//       throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+//     }
+
+//     return _items[index];
+//   }
+// }
+
+var numbers = new List<int> { 5, 3, 2, 8, 16, 7 };
+TwoInts minAndMax = GetMinAndMax(numbers);
+System.Console.WriteLine($"Smallest number is {minAndMax.Int1}");
+System.Console.WriteLine($"Largest number is {minAndMax.Int2}");
 
 Console.ReadKey();
 
-public class SimpleList<T>
+TwoInts GetMinAndMax(List<int> numbers)
 {
-  private T[] _items = new T[4];
-  private int _size = 0;
-
-  public void Add(T item)
+  if (!numbers.Any())
   {
-    if (_size >= _items.Length)
-    {
-      var newItems = new T[_items.Length * 2];
-
-      for (int i = 0; i < _items.Length; i++)
-      {
-        newItems[i] = _items[i];
-      }
-      _items = newItems;
-    }
-    _items[_size] = item;
-    ++_size;
+    throw new InvalidOperationException($"Yo shit is empty buddy.");
   }
 
-  public void RemoveAt(int index)
+  int min = numbers.First();
+  int max = numbers.First();
+
+  foreach (int number in numbers)
   {
-    if (index < 0 || index > _size)
+    if (number > max)
     {
-      throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+      max = number;
     }
-
-    --_size;
-
-    for (int i = index; i < _size; ++i)
+    if (number < min)
     {
-      _items[i] = _items[i + 1];
+      min = number;
     }
-
-    _items[_size] = default;
   }
 
-  public T GetAtIndex(int index)
-  {
-    if (index < 0 || index > _size)
-    {
-      throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
-    }
+  return new TwoInts(min, max);
+}
 
-    return _items[index];
+public class TwoInts
+{
+  public int Int1 { get; }
+  public int Int2 { get; }
+  public TwoInts(int int1, int int2)
+  {
+    Int1 = int1;
+    Int2 = int2;
   }
 }
