c2e5039ec5d15b1209d4974619c6348bc5157397
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-01 11:13:06 -0500

    completed another assignment. learned how to swap index positions in tuples.

--- AlternateLesson/GenericMethods.cs
+++ AlternateLesson/GenericMethods.cs
@@ -1,21 +1,21 @@
 namespace AlternateLesson.GenericMethods
 {
-  public static class ListExtensions
-  {
-    public static void AddToFront<T>(this List<T> list, T item)
-    {
-      list.Insert(0, item);
-    }
+  // public static class ListExtensions
+  // {
+  //   public static void AddToFront<T>(this List<T> list, T item)
+  //   {
+  //     list.Insert(0, item);
+  //   }
 
-    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> decimals)
-    {
-      var result = new List<TSource>();
+  //   public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> decimals)
+  //   {
+  //     var result = new List<TSource>();
 
-      foreach (decimal number in decimals)
-      {
-        result.Add((TTarget)number);
-      }
-      return result;
-    }
-  }
+  //     foreach (decimal number in decimals)
+  //     {
+  //       result.Add((TTarget)number);
+  //     }
+  //     return result;
+  //   }
+  // }
 }
--- Program.cs
+++ Program.cs
@@ -129,7 +129,13 @@
 //   }
 // }
 
-var decimals = new List<decimal> { 1.1m, 0.5m, 22.5m, 12m };
-var ints = decimals.ConvertTo();
+// var decimals = new List<decimal> { 1.1m, 0.5m, 22.5m, 12m };
+// var ints = decimals.ConvertTo();
+TupleSwapExercise.SwapTupleItems(new Tuple<int, int>(1, 2));
 
 Console.ReadKey();
+
+public static class TupleSwapExercise
+{
+  public static Tuple<T2, T1> SwapTupleItems<T1, T2>(Tuple<T1, T2> elements) => new Tuple<T2, T1>(elements.Item2, elements.Item1);
+}
