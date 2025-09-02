49f4afd6e79dec98015f5b22ba882a30d5316904
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-02 13:54:47 -0500

    completed another assignment and fully understand type constraints and IComparable interface.

--- Program.cs
+++ Program.cs
@@ -1,60 +1,104 @@
-﻿var john = new Person(){  Name = "John",  YearOfBirth = 1980};
-var anna = new Person() { Name = "Anna", YearOfBirth = 1915 };
+﻿// var john = new Person(){  Name = "John",  YearOfBirth = 1980};
+// var anna = new Person() { Name = "Anna", YearOfBirth = 1915 };
 
-PrintInOrder(10, 5);
-PrintInOrder("aaa", "bb");
-PrintInOrder(anna, john);
+// PrintInOrder(10, 5);
+// PrintInOrder("aaa", "bb");
+// PrintInOrder(anna, john);
+
+// Console.ReadKey();
+
+// void PrintInOrder<T>(T first, T second) where T : IComparable<T>
+// {
+//   if (first.CompareTo(second) > 0)
+//   {
+//     System.Console.WriteLine($"{second} {first}");
+//   }
+//   else
+//   {
+//     System.Console.WriteLine($"{first} {second}");
+//   }
+// }
+
+// IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> persons) where TPerson : Person
+// {
+//   var result = new List<TPerson>();
+
+//   foreach (var person in persons)
+//   {
+//     if (person.YearOfBirth > 1900 && person.YearOfBirth < DateTime.Now.Year)
+//     {
+//       result.Add(person);
+//     }
+//   }
+//   return result;
+// }
+
+// public class Person : IComparable<Person>
+// {
+//   public string Name { get; init; }
+//   public int YearOfBirth { get; init; }
+
+//   public override string ToString() => $"{Name} was born in {YearOfBirth}"; 
+
+//   public int CompareTo(Person other)
+//   {
+//     if (YearOfBirth < other.YearOfBirth)
+//     {
+//       return 1;
+//     }
+//     else if (YearOfBirth > other.YearOfBirth)
+//     {
+//       return -1;
+//     }
+//     return 0;
+//   }
+// }
+
+// public class Employee : Person
+// {
+//   public void GoToWork() => System.Console.WriteLine("Going to work! Fuck my life!😀");
+// }
+
+List<FullName> listOfNames = new List<FullName>() {
+  new FullName() {FirstName = "John", LastName = "Smith"},
+  new FullName() {FirstName = "Anna", LastName = "Smith"},
+  new FullName() {FirstName = "Kenji", LastName = "Narasaki"},
+  new FullName() {FirstName = "John", LastName = "Watson"}
+};
+var sorted = new SortedList<FullName>(listOfNames);
+foreach (FullName name in sorted.Items)
+{
+  System.Console.WriteLine($"{name}");
+}
 
 Console.ReadKey();
 
-void PrintInOrder<T>(T first, T second) where T : IComparable<T>
-{
-  if (first.CompareTo(second) > 0)
-  {
-    System.Console.WriteLine($"{second} {first}");
-  }
-  else
-  {
-    System.Console.WriteLine($"{first} {second}");
-  }
-}
-
-IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> persons) where TPerson : Person
-{
-  var result = new List<TPerson>();
-
-  foreach (var person in persons)
-  {
-    if (person.YearOfBirth > 1900 && person.YearOfBirth < DateTime.Now.Year)
+public class SortedList<T> where T : IComparable<T>
     {
-      result.Add(person);
+        public IEnumerable<T> Items { get; }
+
+        public SortedList(IEnumerable<T> items)
+        {
+            var asList = items.ToList();
+            asList.Sort();
+            Items = asList;
+        }
     }
-  }
-  return result;
-}
+    
+    public class FullName : IComparable<FullName>
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
 
-public class Person : IComparable<Person>
-{
-  public string Name { get; init; }
-  public int YearOfBirth { get; init; }
-
-  public override string ToString() => $"{Name} was born in {YearOfBirth}"; 
-
-  public int CompareTo(Person other)
+  public int CompareTo(FullName other)
   {
-    if (YearOfBirth < other.YearOfBirth)
+    var resultOfLastName = LastName.CompareTo(other.LastName);
+    if (LastName.CompareTo(other.LastName) != 0)
     {
-      return 1;
+      return resultOfLastName;
     }
-    else if (YearOfBirth > other.YearOfBirth)
-    {
-      return -1;
-    }
-    return 0;
+    else return FirstName.CompareTo(other.FirstName);
   }
-}
 
-public class Employee : Person
-{
-  public void GoToWork() => System.Console.WriteLine("Going to work! Fuck my life!😀");
-}
+  public override string ToString() => $"{FirstName} {LastName}";
+    }
